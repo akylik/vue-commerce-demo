b70dfca329adc3e7d41ef1d0a73ed7ea50794af4
o.kulyk <o.kulyk@ejaw.net>
2024-01-21 19:24:56 +0200

    update list

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,14 +15,17 @@ defineProps({
 
 <template>
   <div
-    class="relative bg-white border border-slate-200 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-    <img v-if="onClickFavorite" @click="onClickFavorite" :src="isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="Like"
-      class="absolute top-8 left-8" />
-    <img :src="imageUrl" alt="Sneakers 1" />
-    <p class="mt-2">{{ title }}</p>
+    class="flex flex-col justify-between relative bg-white border border-slate-200 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
+    <div>
+      <img v-if="onClickFavorite" @click="onClickFavorite" :src="isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="Like"
+        class="absolute top-8 left-8" />
+      <img :src="imageUrl" alt="Sneakers 1" />
+      <p class="mt-2">{{ title }}</p>
+    </div>
+    
 
     <div class="flex justify-between mt-5">
-      <div class="flex flex-col">
+      <div class="flex flex-col ">
         <span class="text-slate-400">Cost</span>
         <b>{{ price }}</b>
       </div>
